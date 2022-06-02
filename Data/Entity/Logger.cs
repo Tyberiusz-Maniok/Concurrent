0b2b9edc9d2590aef8f05b7bca62dda8a99efb17
Wiktor <73667681+MoonGuyPro@users.noreply.github.com>
2022-06-02 20:56:10 +0200

    Logger class

---
+++ Data/Entity/Logger.cs
@@ -0,0 +1,35 @@
+﻿using Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+
+namespace Data
+{
+    public class Logger
+    {
+        private object _lock = new object();
+
+        public void SaveLogsToFile(MovableEntity movableEntity)
+        {
+            var jsonOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            var objectToSerialize = new
+            {
+                Timestamp = DateTime.Now,
+                MovableEntity = movableEntity
+            };
+
+            string json = JsonSerializer.Serialize(objectToSerialize, jsonOptions);
+
+            lock (_lock)
+            {
+                File.AppendAllText(Path.GetFullPath(@"..\..\..\..\logs.json"), json);
+            }
+        }
+    }
+}
